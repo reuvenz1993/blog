3b9f0cdb440dcef1c37eba9fe69f38a5788a95ab
reuvenz1993 <rovenroven1@gmail.com>
2020-01-05 22:15:27 +0200

    --- Auto-Git Commit ---

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from blog import app,db
 from flask import render_template, redirect, request, url_for, flash,abort
+from blog.forms import LoginForm , SignupForm
 
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html')
+    loginform = LoginForm()
+    signupform = SignupForm()
+    return render_template('index.html' , loginform = loginform , signupform = signupform )
 
 if __name__ == '__main__':
     app.run(debug=True)
