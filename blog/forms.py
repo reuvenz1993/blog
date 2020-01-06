3d3dcfe22e67e83ddad773c70138ddc89fcfe4db
reuvenz1993 <rovenroven1@gmail.com>
2020-01-06 19:14:40 +0200

    a

--- blog/forms.py
+++ blog/forms.py
@@ -1,6 +1,6 @@
 from flask_dance.utils import first
 from flask_wtf import FlaskForm
-from wtforms import (IntegerField, PasswordField, StringField, SubmitField,
+from wtforms import (IntegerField, PasswordField, StringField, SubmitField, BooleanField
                      ValidationError)
 from wtforms.validators import DataRequired, Email, EqualTo
 from blog.models import User
@@ -13,36 +13,18 @@ class SignupForm(FlaskForm):
     password = PasswordField('Enter Password: ',validators=[DataRequired()] )
     signup = SubmitField("Sign up")
 
-    def check_email_and_username(self):
-        error =''
-        if User.query.filter_by(email=self.email.data).first() is not None:
-            error +='this email is already in use'
-        if User.query.filter_by(email=self.username.data).first() is not None:
-            error +='this username is already in use'
-        if error =='' :
-            return True
-        else:
-            return error
-
-
-    def check_username(self, field):
-        if User.query.filter_by(username=field.data).first() is not None:
-            return False
-        else:
-            return True
-        
     def reset(self):
         self.email.data = ""
         self.username.data = ""
         self.password.data = ""
 
 
-
 class LoginForm(FlaskForm):
     username = StringField('Enter Username : ' , validators=[DataRequired()] )
     password = PasswordField('Enter Password: ', validators=[DataRequired()] )
+    remember = BooleanField('Remember Me')
     login = SubmitField("Log in")
-    
+
     def reset(self):
         self.username.data = ""
         self.password.data = ""
